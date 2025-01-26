7dacbfe907c59c1de7cb128082538e65ea23ec15
aminbakhshi <bakhshim178@gmail.com>
2025-01-26 22:46:30 +0330

    fix channel...

--- lib/app/pages/home/widgets/mini_live_widget.dart
+++ lib/app/pages/home/widgets/mini_live_widget.dart
@@ -1,11 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:mediaverse/app/common/app_extension.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../../../gen/model/json/FromJsonGetChannelsShow.dart';
 import '../../../common/app_color.dart';
+import '../../../common/app_route.dart';
 
 class MiniLiveWidget extends StatelessWidget {
   ChannelsModel model;
@@ -14,59 +17,68 @@ class MiniLiveWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 20.w,
-      height: 20.w,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: 20.w+1.5.h,
-            child: Stack(
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(5000),
-                    child: Container(
-                      width: 20.w,
-                      height: 20.w,
-                      child: model.thumbnails.toString().length>10?CachedNetworkImage(imageUrl: model.thumbnails['226x226'],fit: BoxFit.cover,
-                        errorWidget: (s,p,q){
-                          return Container(
-                            color: AppColor.backgroundColor,
+    return GestureDetector(
+     onTap: (){
 
-                          );
-                        },):Image.asset("assets/${F.assetTitle}/images/mainLogo.png"),
-                    ),
-                  ),
-                ),
-                Visibility(
-                  visible: model.lastEvent.toString().contains("STARTED"),
-                  child: Align(
+       Get.toNamed(PageRoutes.LIVE ,
+           arguments: {'channelId': model.id});
+
+     },
+
+      child: Container(
+        width: 20.w,
+        height: 20.w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 20.w+1.5.h,
+              child: Stack(
+                children: [
+                  Align(
                     alignment: Alignment.topCenter,
-                    child: Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5000),
+                      child: Container(
+                        width: 20.w,
+                        height: 20.w,
+                        child: model.thumbnails.toString().length>10?CachedNetworkImage(imageUrl: model.thumbnails['226x226'],fit: BoxFit.cover,
+                          errorWidget: (s,p,q){
+                            return Container(
+                              color: AppColor.backgroundColor,
 
-                    margin: EdgeInsets.only(
-                      top: (20.w-(1.5.h/2))
+                            );
+                          },):Image.asset("assets/${F.assetTitle}/images/mainLogo.png"),
+                      ),
                     ),
-                    width: 7.w,
-                    height: 1.5.h,
-                    decoration: BoxDecoration(
-                      color: "B71D18".toColor(),
-                      borderRadius: BorderRadius.circular(3)
-                    ),
-                    child: Center(child: Text("Live",style: TextStyle(fontSize: 6.sp,fontWeight: FontWeight.bold),)),
                   ),
-                ),
-                  )
-              ],
+                  Visibility(
+                    visible: model.lastEvent.toString().contains("STARTED"),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+
+                      margin: EdgeInsets.only(
+                        top: (20.w-(1.5.h/2))
+                      ),
+                      width: 7.w,
+                      height: 1.5.h,
+                      decoration: BoxDecoration(
+                        color: "B71D18".toColor(),
+                        borderRadius: BorderRadius.circular(3)
+                      ),
+                      child: Center(child: Text("Live",style: TextStyle(fontSize: 6.sp,fontWeight: FontWeight.bold),)),
+                    ),
+                  ),
+                    )
+                ],
+              ),
             ),
-          ),
-          Container(
-              width: 15.w,
-              child: Text("${model.name}",maxLines: 1,style: TextStyle(fontSize: 8.sp,fontWeight: FontWeight.bold),overflow: TextOverflow.ellipsis,))
-        ],
+            Container(
+                width: 15.w,
+                child: Text("${model.name}",maxLines: 1,style: TextStyle(fontSize: 8.sp,fontWeight: FontWeight.bold),overflow: TextOverflow.ellipsis,))
+          ],
+        ),
       ),
     );
   }
